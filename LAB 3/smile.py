ee0cbbc4bf2a824cd8e545dcd690ae78b43af0c1
Makinlo <bogdanov.ia@phystech.edu>
2020-09-22 16:45:06 +0300

    smile.py 1_draw.py

--- LAB 3/smile.py
+++ LAB 3/smile.py
@@ -3,11 +3,35 @@ import pygame
 import pygame.draw as d
 
 pygame.init()
-clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1000, 1000))
+FPS = 30
+
+yellow = (255, 255, 0)
+red = (255, 0, 0)
+black = (0,0,0)
+
+d.circle(screen, yellow,(500,300), 150,0)
+d.circle(screen, red, (430,270), 30, 0)
+d.circle(screen, red, (570,270), 20, 0)
+d.circle(screen, black, (430,270), 13, 0)
+d.circle(screen, black, (570,270), 11, 0)
+d.line(screen, black,[])
+
+
+
+
+
+
 pygame.display.update()
-
-fps = 30
 clock = pygame.time.Clock()
-clock(fps)
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
+
