6bdd9227392d6a6138d31b69cda4b7785606d732
Makinlo <bogdanov.ia@phystech.edu>
2020-09-22 16:04:20 +0300

    smile.py

---
+++ LAB 3/smile.py
@@ -0,0 +1,13 @@
+import pygame
+# from pygame.draw import*
+import pygame.draw as d
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.update()
+
+fps = 30
+clock = pygame.time.Clock()
+clock(fps)
