9c553071fde1aee1bba9b5f119fdb0abad12acd8
Makinlo <bogdanov.ia@phystech.edu>
2020-09-25 23:12:33 +0300

    smile.py

--- LAB 3/smile.py
+++ LAB 3/smile.py
@@ -4,25 +4,31 @@ import pygame.draw as d
 
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 1000))
-FPS = 30
-
 yellow = (255, 255, 0)
 red = (255, 0, 0)
 black = (0,0,0)
 
+screen = pygame.display.set_mode((1000, 1000))
+FPS = 30
+screen.fill(red)
+pygame.display.set_caption("Angry toy$")
+pygame.display.flip()
+
+
 d.circle(screen, yellow,(500,300), 150,0)
 d.circle(screen, red, (430,270), 30, 0)
-d.circle(screen, red, (570,270), 20, 0)
+d.circle(screen, red, (570,270), 23, 0)
 d.circle(screen, black, (430,270), 13, 0)
 d.circle(screen, black, (570,270), 11, 0)
-d.line(screen, black,[])
+d.rect(screen, black, (425, 365, 150, 31))
+d.line(screen, black, (350,180), (480,260), 15)
+d.line(screen, black, (520,258), (640,218), 15)
 
 
 
 
 
-
+pygame.display.update()
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -34,4 +40,5 @@ while not finished:
             finished = True
 
 pygame.quit()
+
 
